6b342e62c1c3a31ea2c8ecfc0f39700d9fcf558d
vascorebolo <vasco.s.rebolo@gmail.com>
2023-04-25 13:46:46 +0100

    Add export to output in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
